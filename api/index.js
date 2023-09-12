bc9d83723f9ea8fb28c25da92755ad7164115d82
GrandThed <32280623+GrandThed@users.noreply.github.com>
2023-09-11 23:57:20 -0300

    added styles with bootstrap 5

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const app = require("../app");
 const { PORT } = require("../config");
 require("../db");
-
+// app.listen(PORT, () => console.log("server is running on port", PORT));
 module.exports = app;
